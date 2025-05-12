1da1e0235d08de8e06f361c495ee4123f4d330ca
Kristof0719 <csangcsungcsing@gmail.com>
2025-05-12 14:02:06 +0200

    newtodo meg minden

--- my.js
+++ my.js
@@ -21,7 +21,8 @@ const { createApp } = Vue
             new Todo("todo 3"),
             new Todo("todo 4"),
             new Todo("todo 5")
-        ]
+        ],
+        newTodoName: null
         }
       },
       methods:{
@@ -30,6 +31,18 @@ const { createApp } = Vue
             'my-line-through' : completed,
             'my-red' : completed
             }
+        },
+        onDblClickTodoName(item){
+          item.editing = true;
+        
+        },
+        onEnterTodoName(item){
+          item.editing = false;
+        },
+        onEnterNewTodo(){
+          const newTodo = new Todo(this.newTodoName);
+          this.todoCollection.push(newTodo);
+          this.newTodoName = null;
         }
       }
     }
